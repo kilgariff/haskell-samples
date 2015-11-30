7233b0ac14d1bf6bea14759ff63aee2facbf6472
Ross Kilgariff <rosskilgariff@googlemail.com>
2015-11-30 17:05:59 +0000

    Modified TCP client example to use Async

--- Networking/client.hs
+++ Networking/client.hs
@@ -1,44 +1,66 @@
 -- | Dependencies
 
-import Control.Monad (forever)
-import Network.Socket hiding (send, sendTo, recv, recvFrom)
-import Network.Socket.ByteString (send, recv, sendAll)
-import qualified Data.ByteString.Char8 as B8
+import           Control.Concurrent
+import           Control.Concurrent.Async
+import           Control.Exception
+import qualified Data.ByteString.Char8     as B8
+import           Data.Function             (fix)
+import           Network.Socket            hiding (recv, recvFrom, send, sendTo)
+import           Network.Socket.ByteString (recv, send, sendAll)
+import           System.IO
+import           System.IO.Error
 
 -- | Create a TCP connection to hostname on specified port
 
 connectToHost:: String -> Int -> IO Socket
-connectToHost host port = withSocketsDo $ do
+connectToHost host port = do
+
     addrInfo <- getAddrInfo Nothing (Just host) (Just $ show port)
     let serverAddr = head addrInfo
     sock <- socket (addrFamily serverAddr) Stream defaultProtocol
     connect sock (addrAddress serverAddr)
     return sock
 
--- | Lockstep behaviour
+-- | Create a stdin -> TCP pipe
 
-msgSender :: Socket -> IO ()
-msgSender sock = do
-    msg <- B8.getLine
-    send sock msg
-    if msg == B8.pack "q"
-        then putStrLn "Disconnected!"
-        else do
-            rMsg <- recv sock 10
-            B8.putStrLn rMsg
-            msgSender sock
+pipeInput :: Socket -> IO ()
+pipeInput sock = do
+    result <- tryIOError (B8.getLine >>= sendAll sock)
+    case result of
+        Left _ -> do
+            putStrLn "Got IO Exception while sending"
 
--- | Run a test client
+        Right _ -> pipeInput sock
 
-tester :: IO ()
-tester = do
+-- | Create a TCP -> stdout pipe
+
+pipeOutput :: Socket -> IO ()
+pipeOutput sock = do
+    result <- tryIOError (recv sock 4096)
+    case result of
+        Left _ -> do
+            putStrLn "Got IO Exception while receiving"
+        Right rMsg -> do
+            if B8.length rMsg == 0
+                then do
+                    putStrLn "Connection closed by remote host"
+                else do
+                    B8.putStrLn rMsg
+                    pipeOutput sock
+
+
+-- | Proxy all IO to/from a remote TCP connection
+
+fullDuplexProxy :: IO ()
+fullDuplexProxy = withSocketsDo $ do
 
     -- Create a socket
-    sock <- connectToHost "localhost" 4444
+    sock <- connectToHost "127.0.0.1" 4444
 
-    -- Pipe all input from getLine to the socket
-    forever $ do
-        B8.getLine >>= send sock
+    -- Set up bi-directional (full duplex) pipe
+    race_ (pipeInput sock) (pipeOutput sock)
 
+    -- Clean up
+    putStrLn "Cleaning up"
     sClose sock
     return ()
